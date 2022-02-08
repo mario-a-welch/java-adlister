41432efa3560d51d54c109d99ef210ca097777c1
mario welch <mario.welch01@gmail.com>
2022-02-08 15:13:06 -0600

    sessions-and-cookies-exercise

--- src/main/java/LogOutServlet.java
+++ src/main/java/LogOutServlet.java
@@ -3,15 +3,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet(name = "LogOutServlet", urlPatterns = "/logout")
+public class LogOutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getSession().getAttribute("user") != null){
-            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-        }else
-            response.sendRedirect("/login");
-
+        request.getSession().invalidate();
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 }
